0661d5ea26b7eddedcf85824acc0234a667ebbde
Rick <166161351+DevGameAlias@users.noreply.github.com>
2025-03-14 10:27:43 -0400

    create react app, remove extra code, set up file tructure, add dependencies, test tailwind

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-200">
+      <h1 className="text-4xl font-bold text-blue-500">Tailwind is Working!</h1>
+    </div>
+  );
+}
+
+export default App;
